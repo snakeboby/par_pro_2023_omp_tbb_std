6f4ac169fd4226aaeec937e7bbafe2e3113ff588
snakeboby <81777187+snakeboby@users.noreply.github.com>
2023-05-30 21:18:56 +0300

    style fix

--- modules/task_3/chubenko_a_dijkstra_tbb/dijkstra.h
+++ modules/task_3/chubenko_a_dijkstra_tbb/dijkstra.h
@@ -1,10 +1,10 @@
 // Copyright 2023 Chubenko Andrey
-#ifndef MODULES_TASK_3_CHUBENKO_A_DJIKSTRA_DIJKSTRA_H_
-#define MODULES_TASK_3_CHUBENKO_A_DJIKSTRA_DIJKSTRA_H_
+#ifndef MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_TBB_DIJKSTRA_H_
+#define MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_TBB_DIJKSTRA_H_
 #include <vector>
 
 std::vector<int> dijkstra(const int start,
                           const std::vector<std::vector<int>>& matrix,
                           const int num_threads);
-#endif  // MODULES_TASK_3_CHUBENKO_A_DJIKSTRA_DIJKSTRA_H_
+#endif  // MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_TBB_DIJKSTRA_H_
 
